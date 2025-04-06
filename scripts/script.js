aa5378f0dc6810163e57956c3d5605da5aa35a1f
Chokdee Prushyapong <choikdee50@gmail.com>
2025-04-06 14:47:24 +0700

    add countdown

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
-
+    initCountdown();
 })
 
 let count = 11;
@@ -26,5 +26,37 @@ function addProduct() {
     count += 1;
     const falsh_sale = document.getElementById('flash-sale-id')
     falsh_sale.innerText = `Flash Sale (${count})`
+    
+}
+
+
+function initCountdown() {
+    const countdownElement = document.querySelector('.countdown');
+    const endTime = new Date(Date.now() + 10 * 60 * 60 * 1000); // 24 hours from no
+    const second_text = document.getElementById('sec')
+    const min_text = document.getElementById('min')
+    const hour_text = document.getElementById('hour')
+    function updateCountdown() {
+        const now = new Date();
+        const timeLeft = endTime - now;
+
+        if (timeLeft <= 0) {
+            countdownElement.textContent = "Flash Sale Ended!";
+            clearInterval(countdownInterval);
+            return;
+        }
+
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        hour_text.innerText = hours
+        min_text.innerText = minutes
+        second_text.innerText = seconds
+
+        
+    }
+
+    updateCountdown();
+    const countdownInterval = setInterval(updateCountdown, 1000);
     
 }
